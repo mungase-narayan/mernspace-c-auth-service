cfffabbdd9b4a6196464e3cd7d6911eb7618eb70
mungase-narayan <narayanmungase03@gmail.com>
2024-09-16 08:07:49 +0530

    add exprss js

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,17 @@
+import app from "./app";
 import { Config } from "./config";
 
-console.log("PORT: ", Config.PORT);
+const startServer = async () => {
+  const port = Config.PORT || 5500;
+
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
