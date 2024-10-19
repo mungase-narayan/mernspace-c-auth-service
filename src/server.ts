41436004a943834559da780f324b19fc6b916cfc
mungase-narayan <narayanmungase03@gmail.com>
2024-10-19 17:52:22 +0530

    add database connection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
 import app from "./app";
 import logger from "./config/logger";
 import { Config } from "./config";
-
+import { AppDataSource } from "./config/data-source";
 
 const startServer = async () => {
   const port = Config.PORT || 5500;
 
   try {
+    AppDataSource.initialize()
+      .then(() => {
+        console.log("Data Source has been initialized!");
+        // start the app
+      })
+      .catch((err) => {
+        console.error("Error during Data Source initialization:", err);
+      });
     app.listen(port, () => {
       logger.info(`Server is running on port ${port}`);
     });
@@ -16,7 +24,7 @@ const startServer = async () => {
       setTimeout(() => {
         process.exit(1);
       }, 1000);
-    }
+    } else logger.error("Internal Server Error");
   }
 };
 
