18c9d7a5f2e0da6f0706bbcfd8b2e8c7535a2277
mungase-narayan <narayanmungase03@gmail.com>
2024-10-29 08:08:31 +0530

    add new Entity RefreshToken

---
+++ src/entity/RefreshToken.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  UpdateDateColumn,
+  CreateDateColumn,
+} from "typeorm";
+import { User } from "./User";
+
+@Entity()
+export class RefreshToken {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "timestamp" })
+  expiresAt: Date;
+
+  @ManyToOne(() => User)
+  user: User;
+
+  @UpdateDateColumn()
+  updatedAt: number;
+
+  @CreateDateColumn()
+  createdAt: number;
+
+  constructor(
+    id: number,
+    expiresAt: Date,
+    user: User,
+    updatedAt: number,
+    createdAt: number,
+  ) {
+    this.id = id;
+    this.expiresAt = expiresAt;
+    this.user = user;
+    this.updatedAt = updatedAt;
+    this.createdAt = createdAt;
+  }
+}
