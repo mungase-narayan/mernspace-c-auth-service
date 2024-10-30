c6921f4769feb785f3e410092106717ad742f976
mungase-narayan <narayanmungase03@gmail.com>
2024-10-30 09:31:09 +0530

    add test

--- src/tests/users/login.spec.ts
+++ src/tests/users/login.spec.ts
@@ -26,12 +26,24 @@ describe("POST /auth/login", () => {
   });
 
   describe("Given all fields", () => {
-    it("should return the 200 status code", async () => {
+    it("should return the 201 status code", async () => {
       // Arrange
       const userData = {
-        email: "narayanmungas03@gmail.com",
+        email: "example@gmail.com",
         password: "Mungase1234",
       };
+
+      const user = {
+        role: "customer",
+        firstName: "Narayan",
+        lastName: "Mungase",
+        email: "example@gmail.com",
+        password: "Mungase1234",
+      };
+
+      await request(app as any)
+        .post("/auth/register")
+        .send(user);
 
       // Act
       const response = await request(app as any)
@@ -40,6 +52,63 @@ describe("POST /auth/login", () => {
 
       // Assert
       expect(response.statusCode).toBe(201);
+    });
+
+    it("Should return the access token and refresh token inside a cookie", async () => {
+      // Arrange
+      const userData = {
+        email: "example@gmail.com",
+        password: "Mungase1234",
+      };
+
+      const user = {
+        role: "customer",
+        firstName: "Narayan",
+        lastName: "Mungase",
+        email: "example@gmail.com",
+        password: "Mungase1234",
+      };
+
+      await request(app as any)
+        .post("/auth/register")
+        .send(user);
+
+      // Act
+      const response = await request(app as any)
+        .post("/auth/login")
+        .send(userData);
+
+      // Assert
+      expect(response.statusCode).toBe(201);
+      expect(response.headers["set-cookie"]).toBeTruthy();
+    });
+
+    it("Should return the 400 status code if email or password is invalid", async () => {
+      // Arrange
+      const UserData = {
+        email: "narayanmungas@gmail.com",
+        password: "invalid",
+      };
+
+      const user = {
+        role: "customer",
+        firstName: "Narayan",
+        lastName: "Mungase",
+        email: "example@gmail.com",
+        password: "Mungase1234",
+      };
+
+      await request(app as any)
+        .post("/auth/register")
+        .send(user);
+
+      // Act
+      const response = await request(app as any)
+        .post("/auth/login")
+        .send(UserData);
+
+      // Assert
+      expect(response.statusCode).toBe(400);
     });
   });
 });
