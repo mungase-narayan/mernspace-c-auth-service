70bd668ef0da1b53f4174f4edfb43a473ef0efab
mungase-narayan <narayanmungase03@gmail.com>
2024-11-06 18:09:41 +0530

    add test: 201 status code, create tenant in database, authenticated

--- src/tests/tenants/tenant.spec.ts
+++ src/tests/tenants/tenant.spec.ts
@@ -3,21 +3,34 @@ import { AppDataSource } from "../../config/data-source";
 import request from "supertest";
 import app from "../../app";
 import { Tenant } from "../../entity/Tenant";
+import createJWKSMock from "mock-jwks";
+import { Roles } from "../../constants";
 
 describe("POST /tenants", () => {
   let connection: DataSource;
+  let jwks: ReturnType<typeof createJWKSMock>;
+  let adminToken: string;
+
   beforeAll(async () => {
     try {
+      jwks = createJWKSMock("http://localhost:5501");
       connection = await AppDataSource.initialize();
     } catch (error) {
-      console.error("Error during Database connection", error);
+      console.error("Error during Database connection in test", error);
       throw error;
     }
   });
 
   beforeEach(async () => {
+    jwks.start();
     await connection.dropDatabase();
     await connection.synchronize();
+
+    adminToken = jwks.token({ sub: "1", role: Roles.ADMIN });
+  });
+
+  afterEach(async () => {
+    jwks.stop();
   });
 
   afterAll(async () => {
@@ -37,6 +50,7 @@ describe("POST /tenants", () => {
       // Act
       const response = await request(app as any)
         .post("/tenants")
+        .set("Cookie", [`accessToken=${adminToken}`])
         .send(tenantData);
 
       // Assert
@@ -53,6 +67,7 @@ describe("POST /tenants", () => {
       // Act
       await request(app as any)
         .post("/tenants")
+        .set("Cookie", [`accessToken=${adminToken}`])
         .send(tenantData);
 
       // Assert
@@ -62,6 +77,26 @@ describe("POST /tenants", () => {
       expect(tenants).toHaveLength(1);
       expect(tenants[0].name).toBe(tenantData.name);
       expect(tenants[0].address).toBe(tenantData.address);
+    });
+
+    it("Should return 401 status code if user is not authenticated", async () => {
+      // Arrange
+      const tenantData = {
+        name: "Arnraj Veg Treat",
+        address: "Located on Latur Road Bypass in Barshi",
+      };
+
+      // Act
+      const response = await request(app as any)
+        .post("/tenants")
+        .send(tenantData);
+
+      // Assert
+      const tenantRepository = connection.getRepository(Tenant);
+      const tenants = await tenantRepository.find();
+
+      expect(tenants).toHaveLength(0);
+      expect(response.statusCode).toBe(401);
     });
   });
 });
