97503f0f95eab02013bad3ad2fca6a4558d41f70
mungase-narayan <narayanmungase03@gmail.com>
2024-11-06 20:08:42 +0530

    add: tenant update method, Validation

--- src/Controllers/TenantController.ts
+++ src/Controllers/TenantController.ts
@@ -1,7 +1,9 @@
-import { Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { TenantService } from "../services/TenantService";
 import { CreateTenantRequest } from "../types";
 import { Logger } from "winston";
+import { matchedData, validationResult } from "express-validator";
+import createHttpError from "http-errors";
 
 export default class {
   constructor(
@@ -10,21 +12,60 @@ export default class {
   ) {}
 
   async create(req: CreateTenantRequest, res: Response, next: NextFunction) {
+    // Validation
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+
     const { name, address } = req.body;
     this.logger.debug("Request for creating a new tenant", req.body);
 
     try {
       const tenant = await this.tenantService.create({ name, address });
+      this.logger.info("Tenant has been created", { id: tenant.id });
 
-      this.logger.info("Tenant has been created successfully", {
+      res.status(201).json({
         id: tenant.id,
+        message: "Tenant has been created successfully",
       });
-      res
-        .status(201)
-        .json({ id: tenant.id, message: "Tenant created successfully" });
     } catch (error) {
       next(error);
       return;
+    }
+  }
+
+  async update(req: CreateTenantRequest, res: Response, next: NextFunction) {
+    // Validation
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+
+    const { name, address } = req.body;
+    const tenantId = req.params.id;
+
+    if (isNaN(Number(tenantId))) {
+      next(createHttpError(400, "Invalid url param."));
+      return;
+    }
+
+    this.logger.debug("Request for updating a tenant", req.body);
+
+    try {
+      await this.tenantService.update(Number(tenantId), {
+        name,
+        address,
+      });
+
+      this.logger.info("Tenant has been updated", { id: tenantId });
+
+      res.json({
+        id: Number(tenantId),
+        message: "Tenant has been updated successfully",
+      });
+    } catch (err) {
+      next(err);
     }
   }
 }
