034cf567a6f1dbba4c339ee00263a698d3cfc16a
mungase-narayan <narayanmungase03@gmail.com>
2024-11-06 20:27:28 +0530

    add: service - getAll

--- src/services/TenantService.ts
+++ src/services/TenantService.ts
@@ -12,4 +12,22 @@ export class TenantService {
   async update(id: number, tenantData: ITenant) {
     return await this.tenantRepository.update(id, tenantData);
   }
+
+  async getAll(validatedQuery: TenantQueryParams) {
+    const queryBuilder = this.tenantRepository.createQueryBuilder("tenant");
+
+    if (validatedQuery.q) {
+      const searchTerm = `%${validatedQuery.q}%`;
+      queryBuilder.where("CONCAT(tenant.name, ' ', tenant.address) ILike :q", {
+        q: searchTerm,
+      });
+    }
+
+    const result = await queryBuilder
+      .skip((validatedQuery.currentPage - 1) * validatedQuery.perPage)
+      .take(validatedQuery.perPage)
+      .orderBy("tenant.id", "DESC")
+      .getManyAndCount();
+    return result;
+  }
 }
