76c41d6fbbfd6c798006890de092825e6537e02b
mungase-narayan <narayanmungase03@gmail.com>
2024-11-08 00:21:29 +0530

    register: userRouter

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import logger from "./config/logger";
 import { HttpError } from "http-errors";
+
 import authRouter from "./routes/auth";
 import tenantRouter from "./routes/tenant";
+import userRouter from "./routes/user";
 
 const app = express();
 app.use(express.static("public"));
@@ -18,6 +20,7 @@ app.get("/", async (req, res) => {
 
 app.use("/auth", authRouter);
 app.use("/tenants", tenantRouter);
+app.use("/users", userRouter);
 
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
   logger.error(err.message);
