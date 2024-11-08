71c083e44fdcee403a8c4a3fd8fb347f9fa77174
mungase-narayan <narayanmungase03@gmail.com>
2024-11-08 16:46:16 +0530

    add service: update, getAll, deleteById

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,8 +1,8 @@
 import bcrypt from "bcrypt";
-import { Repository } from "typeorm";
+import { Brackets, Repository } from "typeorm";
 import createHttpError from "http-errors";
 import { User } from "../entity/User";
-import { UserData } from "../types";
+import { LimitedUserData, UserData, UserQueryParams } from "../types";
 
 export class UserService {
   constructor(private userRepository: Repository<User>) {}
@@ -51,5 +51,67 @@ export class UserService {
   async findById(id: number) {
     const user = await this.userRepository.findOne({ where: { id } });
     return user;
+  }
+
+  async update(
+    userId: number,
+    { firstName, lastName, role, email, tenantId }: LimitedUserData,
+  ) {
+    const user = await this.userRepository.findOne({
+      where: { email: email },
+    });
+    if (user) {
+      const err = createHttpError(400, "Email is already exists!");
+      throw err;
+    }
+
+    try {
+      return await this.userRepository.update(userId, {
+        firstName,
+        lastName,
+        role,
+        email,
+        tenant: tenantId ? { id: tenantId } : undefined,
+      });
+    } catch (err) {
+      const error = createHttpError(
+        500,
+        "Failed to update the user in the database",
+      );
+      throw error;
+    }
+  }
+
+  async getAll(validatedQuery: UserQueryParams) {
+    const queryBuilder = this.userRepository.createQueryBuilder("user");
+
+    if (validatedQuery.q) {
+      const searchTerm = `%${validatedQuery.q}%`;
+      queryBuilder.where(
+        new Brackets((qb) => {
+          qb.where("CONCAT(user.firstName, ' ', user.lastName) ILike :q", {
+            q: searchTerm,
+          }).orWhere("user.email ILike :q", { q: searchTerm });
+        }),
+      );
+    }
+
+    if (validatedQuery.role) {
+      queryBuilder.andWhere("user.role = :role", {
+        role: validatedQuery.role,
+      });
+    }
+
+    const result = await queryBuilder
+      .leftJoinAndSelect("user.tenant", "tenant")
+      .skip((validatedQuery.currentPage - 1) * validatedQuery.perPage)
+      .take(validatedQuery.perPage)
+      .orderBy("user.id", "DESC")
+      .getManyAndCount();
+    return result;
+  }
+
+  async deleteById(userId: number) {
+    return await this.userRepository.delete(userId);
   }
 }
