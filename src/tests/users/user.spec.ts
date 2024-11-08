945e22da25a2afff9ec7f0b84c72d9da9710f87f
mungase-narayan <narayanmungase03@gmail.com>
2024-11-08 09:52:29 +0530

    add test: create a manager user

--- src/tests/users/user.spec.ts
+++ src/tests/users/user.spec.ts
@@ -48,6 +48,7 @@ describe("POST /users", () => {
         email: "example@gmail.com",
         password: "Mungase1234",
         role: Roles.MANAGER,
+        // tenantId: 1,
       };
       // Add token to cookie
       const response = await request(app as any)
@@ -63,6 +64,32 @@ describe("POST /users", () => {
       expect(users[0].firstName).toBe(userData.firstName);
       expect(users[0].lastName).toBe(userData.lastName);
       expect(users[0].email).toBe(userData.email);
+    });
+
+    it("Should create a manager user", async () => {
+      const adminToken = jwks.token({ sub: "1", role: Roles.ADMIN });
+
+      // Register user
+      const userData = {
+        firstName: "Narayan",
+        lastName: "Mungase",
+        email: "example@gmail.com",
+        password: "Mungase1234",
+        role: Roles.MANAGER,
+        // tenantId: 1,
+      };
+      // Add token to cookie
+      const response = await request(app as any)
+        .post("/users")
+        .set("Cookie", [`accessToken=${adminToken}`])
+        .send(userData);
+
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+
+      expect(response.statusCode).toBe(201);
+      expect(users).toHaveLength(1);
+      expect(users[0].role).toBe(userData.role);
     });
 
     it("Should return 403 if non admin user tries to create user", async () => {});
