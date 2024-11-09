e58608413982ec971ec762a694215e06e8f4d1d3
mungase-narayan <narayanmungase03@gmail.com>
2024-11-09 11:13:22 +0530

    refactoring

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+// eslint-disable-next-line no-undef
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
   verbose: true,
+  collectCoverage: true,
+  coverageProvider: "v8",
+  collectCoverageFrom: ["src/**/*.ts", "!tests/**", "!**/node_modules/**"],
 };
