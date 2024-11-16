856dba6d672d7aa5263a67c468b9a06ce21dc998
mungase-narayan <narayanmungase03@gmail.com>
2024-11-16 19:24:55 +0530

    replace bcrypt with bcryptjs

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { Brackets, Repository } from "typeorm";
 import createHttpError from "http-errors";
 import { User } from "../entity/User";
