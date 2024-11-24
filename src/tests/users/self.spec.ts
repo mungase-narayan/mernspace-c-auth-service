9cc5e86b12b75915de17eab6a9307af6e4e6b308
mungase-narayan <narayanmungase03@gmail.com>
2024-11-24 19:27:03 +0530

    remove one test

--- src/tests/users/self.spec.ts
+++ src/tests/users/self.spec.ts
@@ -38,17 +38,6 @@ describe("GET /auth/self", () => {
   });
 
   describe("Given all fields", () => {
-    it("Should return the 200 status code", async () => {
-      const accessToken = jwks.token({ sub: "1", role: Roles.CUSTOMER });
-
-      const response = await request(app as any)
-        .get("/auth/self")
-        .set("Cookie", [`accessToken=${accessToken}`])
-        .send();
-
-      expect(response.statusCode).toBe(200);
-    });
-
     it("Should return the user data", async () => {
       const userData = {
         firstName: "Narayan",
