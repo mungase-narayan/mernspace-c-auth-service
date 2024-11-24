04be172453fdeff07cbaa929bb1a17a5104e5096
mungase-narayan <narayanmungase03@gmail.com>
2024-11-24 19:41:08 +0530

    refactor self controller function

--- src/Controllers/AuthController.ts
+++ src/Controllers/AuthController.ts
@@ -156,24 +156,8 @@ export class AuthController {
   }
 
   async self(req: AuthRequest, res: Response) {
-    try {
-      if (!req.auth || !req.auth.sub) {
-        return res
-          .status(400)
-          .json({ message: "User ID is missing in auth data." });
-      }
-
-      const user = await this.userService.findById(Number(req.auth.sub));
-
-      if (!user) {
-        return res.status(404).json({ message: "User not found." });
-      }
-
-      res.status(200).json({ ...user, password: undefined });
-    } catch (error) {
-      console.error("Error in self method:", error);
-      res.status(500).json({ message: "Internal Server Error" });
-    }
+    const user = await this.userService.findById(Number(req.auth.sub));
+    res.status(200).json({ ...user, password: undefined });
   }
 
   async refresh(req: AuthRequest, res: Response, next: NextFunction) {
